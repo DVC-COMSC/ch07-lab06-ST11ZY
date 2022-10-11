8e88cbb76a34729835f60dda8fd6adadf8fad2bb
Your Name <you@example.com>
2022-10-10 22:39:02 -0700

    test

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 
 inputvalues = input('Enter all elements values: ')
 numbers1 = inputvalues.split() 
-for i in range(len(numbers1)):
+numbers2=[]
+y=0
+x=len(numbers1)
+for i in range(x):
 	numbers1[i] = int(numbers1[i]) 
+
 # The following line is the same as the for-loop
 # numbers1 = list(map(int, numbers))
 
-# print ("The original list: ", numbers1)
-
+print ("The original list: ", numbers1)
 # ******************************
 # Make your Code
 # ******************************
+x=x-1
+while x>=0:
+	numbers2.insert(y,numbers1[x])
+	x=x-1
+	y=y+1
+print("New list:", numbers2)
